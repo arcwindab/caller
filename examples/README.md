81c1d987d71d2c021b0985945c688ec7c4253333
Tobias Jonson <support@bot.arcwind.se>
2021-10-05 23:28:46 +0200

    New examples

--- examples/README.md
+++ examples/README.md
@@ -2,3 +2,12 @@
 
 ## example_simple-get.php
 The simplest method get($url) send a GET-request and returns the content of that URL as a string.
+```
+echo $caller->get('https://filesamples.com/samples/document/txt/sample1.txt');
+```
+
+## example_custom_user-agent.php
+The set_useragent method allow you to set a custom User agent.
+```
+$caller->set_useragent('ArcWind/'.$caller->get_version().' (Crawler)');
+```
---
+++ examples/example_custom_user-agent.php
@@ -0,0 +1,16 @@
+<?php include_once '../src/caller.class.php';
+
+/** Initiate caller class
+ */
+$caller = new arcwindab\caller();
+
+
+/** The set_useragent method allow you to set 
+ * a custom User agent.
+ */
+$caller->set_useragent('ArcWind/'.$caller->get_version().' (Crawler)');
+
+/** The simplest method get($url) send a 
+ * GET-request and returns the content of that URL as a string.
+ */
+echo $caller->get('https://filesamples.com/samples/document/txt/sample1.txt');
