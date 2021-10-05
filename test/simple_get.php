e96ab7a60ecefb520badeec1459a551a004b1261
ArcWind AB <91745686+arcwindab@users.noreply.github.com>
2021-10-05 23:34:57 +0200

    Update and rename test.php to simple_get.php

---
+++ test/simple_get.php
@@ -0,0 +1,11 @@
+<?php include_once '../src/caller.class.php';
+
+/** Initiate caller class
+ */
+$caller = new arcwindab\caller();
+
+
+/** The simplest method get($url) send a 
+ * GET-request and returns the content of that URL as a string.
+ */
+echo $caller->get('https://filesamples.com/samples/document/txt/sample1.txt');
