947bfee37535c8fdf30a06822020006065877515
ArcWind AB <91745686+arcwindab@users.noreply.github.com>
2021-10-17 22:14:23 +0200

    Update simple_get.php

--- test/simple_get.php
+++ test/simple_get.php
@@ -4,7 +4,7 @@
  */
 $caller = new arcwindab\caller();
 $caller->set_useragent('ArcWind/'.$caller->get_version());
-
+$caller->set_ip('8.8.8.8');
 
 /** The simplest method get($url) send a 
  * GET-request and returns the content of that URL as a string.
